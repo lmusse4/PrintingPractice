822476623a77b6ba10bcbcbb4ecf3d423df49938
Leyla Musse <lmusse4@gmail.com>
2023-05-08 18:49:23 -0500

    Printing exercise

---
+++ src/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String greeting = "Hi " + "Larry " + "Sprinkle!";
+        System.out.println(greeting);
+    }
+}
---
+++ src/MyNameIs.java
@@ -0,0 +1,7 @@
+public class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("My name is");
+        System.out.println("Leyla");
+        System.out.println("Musse");
+    }
+}
